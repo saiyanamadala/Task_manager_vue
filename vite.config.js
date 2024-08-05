c769f1febce0aa95155bab8347e3146c53a6292d
Naga Venkata Sairam Yanamadala <ynvsr.25@gmail.com>
2024-08-05 18:52:18 -0500

    script changes

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build:{
+    outDir: 'dist'
+  },
+  base: '/Task_manager_vue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
